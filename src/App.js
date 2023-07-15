738a76edaa5f36ad412a519a03db5e5b2fd3eeaa
System Administrator <sakhauri.cse18@chitkarauniversity.edu.in>
2023-07-15 19:46:38 +0530

    Initial Profile Commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <h3>
+          Welcome to Sukrit Profile.
+          <br/>
+          Work under progress......
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
